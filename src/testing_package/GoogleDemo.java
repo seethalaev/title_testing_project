bacbe6d220763ff0ba593d5890f24b82abc35d1d
seethala e v <sheethalev@gmail.com>
2024-08-09 22:28:17 +0530

    title verificaiont of web page

--- src/testing_package/GoogleDemo.java
+++ src/testing_package/GoogleDemo.java
@@ -7,6 +7,22 @@ public class GoogleDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		
+		//driver.close()'' //close the window
+		String title = driver.getTitle();
+		String expected_title = "Google";
+		
+		if (title.equals(expected_title) )
+		{
+			System.out.println("test passed");
+		}
+		else
+		{
+			System.out.println("test failed ");
+		}
+		System.out.println("title:"+title);
+		driver.quit(); //quit all the tabs
  
 	}
 
